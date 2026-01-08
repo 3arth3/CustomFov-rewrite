0011a45bf07744a46f1100caa605ca91214130b8
3 <168254582+3arth3@users.noreply.github.com>
2026-01-08 11:41:59 +0700

    Update MixinGameRenderer.java

--- src/main/java/me/aidrob/MixinGameRenderer.java
+++ src/main/java/me/aidrob/MixinGameRenderer.java
@@ -1,7 +1,6 @@
 package me.aidrob.mixin;
 
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,12 +10,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinGameRenderer {
 
     @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
-    private void onGetFov(CallbackInfoReturnable<Double> cir) 
+    private void onGetFov(CallbackInfoReturnable<Double> cir) {
         double originalFov = cir.getReturnValue();
-        double customFov = (originalFov - 30.0) * (200.0 / (110.0 - 30.0));
         
-        if (customFov < 0) customFov = 0;
-        if (customFov > 200) customFov = 200;
+        double customFov = (originalFov - 30.0) * (180.0 / (110.0 - 30.0));
+
+        if (customFov < 0.0) customFov = 0.0;
+        if (customFov > 180.0) customFov = 180.0;
 
         cir.setReturnValue(customFov);
     }
